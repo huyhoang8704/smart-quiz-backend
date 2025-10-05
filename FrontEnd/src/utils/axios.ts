547ddc11ecaf0071d84419498c6949d812f99831
Đào Duy Tùng <duytung.developer@gmail.com>
2025-10-05 15:31:34 +0700

    Update

--- FrontEnd/src/utils/axios.ts
+++ FrontEnd/src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL, // Your API base URL
+    baseURL: "https://quanlyduanapi.duytung-developer.io.vn", // Your API base URL
 });
 
 axiosInstance.interceptors.request.use(
